3395ff7dbe991f2967d1f8c6e77fdceb8720bddb
mohamed <mohade@mohade-mackpro.ie.deri.local>
2017-08-09 15:54:53 +0100

    updates

--- GOVDAF.py
+++ GOVDAF.py
@@ -20,7 +20,7 @@ tweets_collection_name = "policy_analysis2"
 global policy
 policy = Policy()
 
-pfd_path = "/home/mohade/3-workspace/gov_daf_masters/inputs/policies/1.pdf"
+pfd_path = "/Users/mohade/GoogleDrive/__MYPERSONALBACKUP/TEMP-WAITING-ORGANIZATION/3-workspace/GOV-DAF/inputs/policies/1.pdf"
 #/home/mohade/workspace/masters-jupyter/masters_code/inputs/policies/1.pdf
 
 policy.extraction(pfd_path)
@@ -33,16 +33,21 @@ mongo = MongoTasks()
 
 collection = mongo.connect(mongo_config.host, mongo_config.port, policy_db_name, policy_collection_name+"_extracted")
 mongo.appendDataToMongo(collection, policy.getAsJson())
+print ("Public Policy Information Extracted form .pdf to json, and pushed to mogodb Succefully!")
 
 """step#3:Data Cleaning"""
 
 policy.cleaning()
+
 print policy.policy_cleaned_as_list_by_lines_or_pages
+
 
 """step#4:Push extracted and cleaned policy text to mongo """
 
 collection = mongo.connect(mongo_config.host, mongo_config.port, policy_db_name, policy_collection_name+"_extracted_cleaned")
 mongo.appendDataToMongo(collection, policy.getAsJson())
+print ("Public Policy Cleaned and pushed to mogodb Succefully!")
+
 
 """step#5:Topic Modelig Gensim"""
 
@@ -62,98 +67,4 @@ for z in policy.policy_topic_terms:
 
 collection = mongo.connect(mongo_config.host, mongo_config.port, policy_db_name, policy_collection_name+"_extracted_cleaned_topicmodeled")
 mongo.appendDataToMongo(collection, policy.getAsJson())
-
-"""step#7:Correlation Matrix Building"""
-
-global correlation_cell
-correlation_cell = Correlation_cell()
-
-correlationsMatrix = correlation_cell.build_correlation_matrix(policy.policy_topic_terms)
-print correlationsMatrix
-
-#.correlation_matrix_to_csv("outputs/matrix_", correlationsMatrix)
-
-"""step#7:Push Matrix to Mongo"""
-collection = mongo.connect(mongo_config.host, mongo_config.port, policy_db_name, policy_collection_name+"_correlationMatrix")
-for cell in correlationsMatrix:
-    mongo.appendDataToMongo(collection, cell.getAsJson())
-
-"""step#8:query tweets dataset by correlation matrix"""
-db = mongo.connect_to_db(mongo_config.host, mongo_config.port, tweets_db_name)
-temp_list = []
-for cell in correlationsMatrix:
-    cursur = mongo.run_command_search(db, "text","policy_analysis2",cell.get_related_tweets_query())
-    print cell.term_A
-    print cell.term_B
-    for r in cursur["results"]:
-        #print json.dumps(r, indent=4, sort_keys=True)
-        temp_list.append(copy.deepcopy(r["obj"]["id"]))
-        #
-        print r["obj"]["id"]
-        #pprint.pprint(
-    cell.related_tweets_ids = copy.deepcopy(temp_list)
-    del temp_list [:]
-
-"""step#9:Push Matrix to Mongo with related_tweets_ids"""
-collection = mongo.connect(mongo_config.host, mongo_config.port, policy_db_name, policy_collection_name+"_correlationMatrix_with_tweets3")
-for cell in correlationsMatrix:
-    mongo.appendDataToMongo(collection, cell.getAsJson())
-
-"""step#10:Calculate sentiments"""
-global sentiment_analysis
-sentiment_analysis = SentimentAnalysis()
-sentiment_analysis.activate_nuig_sentiment_deep_learining_run_only_once()
-tweet_text = ""
-tweets_collection = mongo.connect(mongo_config.host, mongo_config.port, tweets_db_name, tweets_collection_name)
-
-
-for cell in correlationsMatrix:
-    #print ""
-    #getting tweet text
-    for tweet_id in cell.related_tweets_ids:
-        tweet_cursor = mongo.get_tweet_text(tweets_collection,tweet_id)
-        print tweet_cursor
-        tweet_text = tweet_cursor["text"]
-        #passing tweet text to sentiment service
-        sentiment_result = sentiment_analysis.analyze_Tweet(tweet_text, "deep_learning")
-        sentiment_label = sentiment_result["entries"][0]["sentiments"][0]["marl:hasPolarity"]
-        #saving sentiment analysis result in cell.neg/pos/nutral
-        print sentiment_label
-        if sentiment_label == "positive":
-            cell.positive_tweets_ids.append(copy.deepcopy(tweet_id))
-        if sentiment_label == "negative":
-            cell.negative_tweets_ids.append(copy.deepcopy(tweet_id))
-        if sentiment_label == "neutral":
-            cell.neutral_tweets_ids.append(copy.deepcopy(tweet_id))
-
-"""step#11:Push Matrix to Mongo with sentiments"""
-collection = mongo.connect(mongo_config.host, mongo_config.port, policy_db_name, policy_collection_name+"_correlationMatrix_with_sentiments")
-for cell in correlationsMatrix:
-    mongo.appendDataToMongo(collection, cell.getAsJson())
-
-"""step#12:Satisfaction index calculation"""
-
-global citizendatisfactionanalysis
-citizendatisfactionanalysis = CitizenSatisfactionAnalysis()
-
-citizendatisfactionanalysis.calculate_predict_satisfaction(correlationsMatrix)
-#print correlationsMatrix_Csatisfaction_calc
-
-
-"""step#13:Push Matrix to Mongo with sentiments and calculated satisfaction index"""
-collection = mongo.connect(mongo_config.host, mongo_config.port, policy_db_name, policy_collection_name+"_correlationMatrix_sentiment_satisfaction")
-for cell in citizendatisfactionanalysis.matrix_prediction_ready:
-    mongo.appendDataToMongo(collection, cell.getAsJson())
-
-
-"""step#14:prediction fuction model building"""
-
-"""step#15:prediction fuction model testing"""
-
-"""step#16:system evaluation"""
-
-
-
-
-
 
--- README.md
+++ README.md
@@ -1,3 +1,47 @@
 # GOV-DAF
+=========
+
 Government Satisfaction Analysis Framework
 *initial commits (work on progress)
+
+mongodb installation [one time only]
+====================
+[1] https://docs.mongodb.com/master/administration/install-community/
+[2] https://robomongo.org/download [optional]
+
+
+
+Gov-DAF installation [one time only]
+====================
+$ virtualenv venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+
+NLTK installation [one time only]
+=================
+ http://www.nltk.org/data.html
+[OR]
+$ source venv/bin/activate
+$ python
+$ import nltk
+$ nltk.download()
+then choose "popular packages" from the list.
+![img](screen_samples/nltk.png)
+
+Run Gov-Daf
+===========
+[1] run mongo
+
+$ sudo mongod
+
+[2] append tweet repository to mongodb
+
+[3] set Gov-DAF arguments
+
+[4] run Gov-DAF
+
+[5] print reports
+
+screen samples
+==============
+![img](screen_samples/nltk.png)
